cbc5e34e6d74507a83da25e9c4a72adb9be02900
miqueiasilva <mykeias@gmail.com>
2026-05-12 22:57:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: []
+      },
       manifest: {
         name: 'BelareStudio',
         short_name: 'Belare',
